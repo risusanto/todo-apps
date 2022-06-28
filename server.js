ecb901bdfb0f400a94204453e73b89994c98489e
Ari Susanto <risusanto@outlook.com>
2022-06-28 22:55:34 +0700

    change port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const route = require('./routes')
 require('dotenv').config()
 const app = express()
-const port = process.env.APP_PORT || 3000
+const port = process.env.PORT || 3000
 
 route.New(app)
 app.listen(port, () => {
