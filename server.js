a3924208bcf5deb50eeef3926678fa2c0e02ed70
Ari Susanto <risusanto@outlook.com>
2022-06-28 23:06:43 +0700

    add root path

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const app = express()
 const port = process.env.PORT || 3000
 
 route.New(app)
+
+app.get("/", (req, res) => {
+    res.json({
+        "success": true
+    })
+})
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
